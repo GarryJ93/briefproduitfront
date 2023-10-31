ebee002179e0d3dd04bd3e94536ad92405a60324
GarryJ93 <garry.joly@gmail.com>
2023-10-31 11:54:33 +0100

    config headers requetes protegees

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token');
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', 'Bearer ' + token);
+    }
+    return headers;
+  }
 
   getAllProduct(): Observable<Product[]> {
     return this.http.get<Product[]>('http://localhost:3000/api/produit');
@@ -18,17 +27,31 @@ export class ProductService {
   }
 
   addProduct(Product: Partial<Product>): Observable<Partial<Product>> {
-    return this.http.post<Product>('http://localhost:3000/api/produit', Product);
+    
+    return this.http.post<Product>(
+      'http://localhost:3000/api/produit',
+      Product,
+      { headers: this.getHeaders() }
+    );
   }
 
-  updateProduct(id: number, updateData: Partial<Product>): Observable<Partial<Product>> {
+  updateProduct(
+    id: number,
+    updateData: Partial<Product>
+  ): Observable<Partial<Product>> {
+    console.log(this.getHeaders());
+    
     return this.http.patch<Product>(
       `http://localhost:3000/api/produit/${id}`,
-      updateData
+      updateData,
+      { headers: this.getHeaders() }
     );
   }
 
   deleteProduct(id: number): Observable<Product> {
-    return this.http.delete<Product>(`http://localhost:3000/api/produit/${id}`);
+    return this.http.delete<Product>(
+      `http://localhost:3000/api/produit/${id}`,
+      { headers: this.getHeaders() }
+    );
   }
 }
